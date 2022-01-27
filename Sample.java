8e2c3dc9273d136e727d9fcf7d9f1fa3b827ecaf
EC2 Default User <ec2-user@ip-172-31-14-76.ap-south-1.compute.internal>
2022-01-27 16:37:03 +0000

    my first commit

--- Sample.java
+++ Sample.java
@@ -3,7 +3,7 @@ package day1.examples;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Yash Bajaj DevOps Session");
+		System.out.println("Welcome to Yash Bajaj DevOps Session ...... Hope you are enjoying this session");
 
 	}
 
